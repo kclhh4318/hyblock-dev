9895bdab28a064ff66b47eb9840eca0599561e61
이현호 <lehyno@hynobook.local>
2024-11-05 16:39:07 +0900

    week1 initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+    const Greeter = await hre.ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, Holesky!");
+
+    await greeter.deployed();
+    console.log("Greeter deployed to:", greeter.address);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
